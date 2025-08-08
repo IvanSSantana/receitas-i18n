110629be0fd2cdcc0bcbfed3606dc9b1206f8757
IvanSSantana <ivansantospriv@gmail.com>
2025-08-08 10:31:10 -0300

    Iniciando lógica

---
+++ script.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('.btn-linguas button');
+const elementsToTranslate = document.querySelectorAll('[data-i18n]');
+
+function loadLanguage(lang) {
+  fetch(`i18n/${lang}.json`)
+    .then(response => response.json()) 
+    .then(translations => {
+      elementsToTranslate.forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        if (translations[key]) element.textContent = translations[key];
+      });
+      const img = document.getElementById("imagem");
+      img.src = translations["imagem"];
+
+      localStorage.setItem('linguagem', lang);
+    });
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const lang = button.getAttribute('linguagem');
+    loadLanguage(lang);
+  });
+});
+
+const savedLang = localStorage.getItem('linguagem') || 'pt-br';
+loadLanguage(savedLang);
